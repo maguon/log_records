b5e173a044986a6f3bd96c701e7cdce4576bf5ce
maguon <info@myxxjs.com>
2018-09-26 15:32:26 +0800

    update restify 4.3.4

--- server.js
+++ server.js
@@ -46,30 +46,34 @@ function createServer() {
     // Handles annoying user agents (curl)
     server.pre(restify.pre.userAgentConnection());
 
-    server.use(restify.throttle({
+    server.use(restify.plugins.throttle({
         burst: 100,
         rate: 50,
         ip: true
     }));
+
+
+
     restify.CORS.ALLOW_HEADERS.push('auth-token');
     restify.CORS.ALLOW_HEADERS.push('user-name');
     restify.CORS.ALLOW_HEADERS.push('user-type');
     restify.CORS.ALLOW_HEADERS.push('user-id');
-    restify.CORS.ALLOW_HEADERS.push('Access-Control-Allow-Origin');
+    restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Origin");
+    restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Credentials");
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Methods","GET");
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Methods","POST");
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Methods","PUT");
     restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Methods","DELETE");
-    restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Headers","x-requested-with,content-type");
+    restify.CORS.ALLOW_HEADERS.push("Access-Control-Allow-Headers","accept,api-version, content-length, content-md5,x-requested-with,content-type, date, request-id, response-time");
     server.use(restify.CORS());
-
-
-    server.use(restify.bodyParser({uploadDir:__dirname+'/../uploads/'}));
-    server.use(restify.acceptParser(server.acceptable));
-    server.use(restify.dateParser());
-    server.use(restify.authorizationParser());
-    server.use(restify.queryParser());
-    server.use(restify.gzipResponse());
+    // Use the common stuff you probably want
+    //hard code the upload folder for now
+    server.use(restify.plugins.bodyParser({uploadDir:__dirname+'/../uploads/'}));
+    server.use(restify.plugins.acceptParser(server.acceptable));
+    server.use(restify.plugins.dateParser());
+    server.use(restify.plugins.authorizationParser());
+    server.use(restify.plugins.queryParser());
+    server.use(restify.plugins.gzipResponse());
 
     // static files: /, /index.html, /images...
     //var STATIS_FILE_RE = /\/?\.css|\/?\.js|\/?\.png|\/?\.jpg|\/?\.gif|\/?\.jpeg|\/?\.less|\/?\.eot|\/?\.svg|\/?\.ttf|\/?\.otf|\/?\.woff|\/?\.pdf|\/?\.ico|\/?\.json|\/?\.wav|\/?\.mp3/;
